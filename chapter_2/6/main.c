33c56b6677b9b3c944227022ee63f11763d0d0b5
Mobydack <devzetsubou@gmail.com>
2020-06-05 03:04:48 +0300

    add chapter-2_6

---
+++ chapter_2/6/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int toes, toes_square, toes_comp;
+
+  toes = 10;
+  toes_square = toes * toes;
+  toes_comp = toes * 2;
+
+  printf("Square: %d\n", toes_square);
+  printf("Composition: %d\n", toes_comp);
+
+  return 0;
+}
+
