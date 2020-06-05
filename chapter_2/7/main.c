ec7bd0034608772394c5c3333c65f9f087d6a506
Mobydack <devzetsubou@gmail.com>
2020-06-05 03:18:09 +0300

    add chapter-2_7

---
+++ chapter_2/7/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void printSmile();
+
+int main()
+{
+  printSmile();
+  printSmile();
+  printSmile();
+  printf("\n");
+  printSmile();
+  printSmile();
+  printf("\n");
+  printSmile();
+  printf("\n");
+
+  return 0;
+}
+
+void printSmile()
+{
+  printf("Smile!");
+}
